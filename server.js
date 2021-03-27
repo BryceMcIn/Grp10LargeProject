a1a0ef6fac001334d6e7040abdbfc9306024ae41
Bryce McIntosh <brycemcintosh@knights.ucf.edu>
2021-03-27 14:42:37 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.set('port',(process.env.PORT || 5000));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) =>
+{
+    res.setHeader('Acess-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}.');
+});
+
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static('frontend/build'));
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+    });
+}
