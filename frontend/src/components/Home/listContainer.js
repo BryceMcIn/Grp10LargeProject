389e6e178006281539440da38a96872601b4316b
Bryce McIntosh <brycemcintosh@knights.ucf.edu>
2021-04-14 10:25:22 -0400

    display list items

--- frontend/src/components/Home/listContainer.js
+++ frontend/src/components/Home/listContainer.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 export default function listContainer(props) {
 
+    const displayItems = (props) => {
     const {listItems} = props;
-    console.log(listItems);
     
+    if (listItems > 0){
+        return(
+            listItems.map((item, index) => {
+                console.log(note);
+                return(
+                    <h3>{item.itemTitle}</h3>
+                )
+            })
+        )
+    } else {
+        return (<h3>Nothing????</h3>)
+    }
+}
 return(
     <>
+        {displayItems(props)}
     </>
 )
 }
