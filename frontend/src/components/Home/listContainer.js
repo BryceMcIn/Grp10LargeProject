d0a59acf1fc8833785f2d300b9bdab5cf8b1533d
Bryce McIntosh <brycemcintosh@knights.ucf.edu>
2021-04-14 10:12:27 -0400

    display list items

--- frontend/src/components/Home/listContainer.js
+++ frontend/src/components/Home/listContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export default function listContainer(props) {
-console.log(props.listItems);
-console.log(props)
+
+    const {listItems} = props;
+    console.log(listItems);
+    
 return(
     <>
     </>
