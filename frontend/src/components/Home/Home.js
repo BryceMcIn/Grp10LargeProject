f40b2d60e838b9c5d493d99975f77ce2678b5922
Bryce McIntosh <brycemcintosh@knights.ucf.edu>
2021-04-14 09:16:10 -0400

    display list items

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './home.css';
 import { withRouter } from 'react-router-dom';
