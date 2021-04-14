9be8e3357008c7cc0603e72068ff2c1440227327
Bryce McIntosh <brycemcintosh@knights.ucf.edu>
2021-04-14 09:12:52 -0400

    display list items

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -8,7 +8,6 @@ import { faList } from '@fortawesome/free-solid-svg-icons'
 import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
-import { response } from 'express';
 import listItem from './listItem';
 
 function Home(props){
