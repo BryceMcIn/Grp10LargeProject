8ac400d39dd721e06b4598da4d04a72bfc4b1766
Ro-Hanna Jowallah <rohannajowallah@gmail.com>
2021-04-16 20:01:54 -0400

    Install dependencies / page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,73 @@
-import React, {useState} from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import LoginForm from './components/LoginForm/LoginForm';
-import RegistrationForm from './components/RegistrationForm/RegistrationForm';
-import Home from './components/Home/Home';
-import PrivateRoute from './utils/PrivateRoute';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import AlertComponent from './components/AlertComponent/AlertComponent';  
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import LoginForm from "./components/LoginForm/LoginForm";
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import Home from "./components/Home/Home";
+import PrivateRoute from "./utils/PrivateRoute";
+import Email from "./components/Email";
+import Navbar from "./components/Navbar";
+import Name from "./components/Name";
+import Password from "./components/Password";
+import Friends from "./components/Friends";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AlertComponent from "./components/AlertComponent/AlertComponent";
 function App() {
   const [title, updateTitle] = useState(null);
   const [errorMessage, updateErrorMessage] = useState(null);
   return (
     <Router>
-    <div className="App">
-          <Switch>
-            <Route path="/" exact={true}>
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
-            </Route>
-            <Route path="/register">
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
-            </Route>
-            <Route path="/login">
-              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
-            </Route>
-            <Route path="/home">
-              <Home showError={updateErrorMessage} updateTitle={updateTitle}/>
-            </Route>
-          </Switch>
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact={true}>
+            <RegistrationForm
+              showError={updateErrorMessage}
+              updateTitle={updateTitle}
+            />
+          </Route>
+          <Route path="/register">
+            <RegistrationForm
+              showError={updateErrorMessage}
+              updateTitle={updateTitle}
+            />
+          </Route>
+          <Route path="/login">
+            <LoginForm
+              showError={updateErrorMessage}
+              updateTitle={updateTitle}
+            />
+          </Route>
+          <Route path="/home">
+            <Home showError={updateErrorMessage} updateTitle={updateTitle} />
+          </Route>
+          <Route path="/friends">
+            <Friends showError={updateErrorMessage} updateTitle={updateTitle} />
+          </Route>
+
+          <Route path="/email">
+            <Email />
+          </Route>
+          <Route path="/name">
+            <Name />
+          </Route>
+          <Route path="/password">
+            <Password />
+          </Route>
+
+          <PrivateRoute path="/home">
+            <Home />
+          </PrivateRoute>
+          <PrivateRoute path="/home">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+
+        <Navbar />
+        <AlertComponent
+          errorMessage={errorMessage}
+          hideError={updateErrorMessage}
+        />
+      </div>
     </Router>
   );
 }
